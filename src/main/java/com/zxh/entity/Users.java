c0e669866c7aa4fc8f6165c889678ccb9723761d
zxh <416270713@qq.com>
2019-01-26 10:25:37 +0800

    添加实体类Users

---
+++ src/main/java/com/zxh/entity/Users.java
@@ -0,0 +1,42 @@
+package com.zxh.entity;
+
+import java.util.Date;
+
+public class Users {
+    private String name;
+    private String password;
+    private int age;
+    private Date birth;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+}
